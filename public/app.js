f2bb5ed84bfdd57a8a69644158748ea46af0588b
Tiago.Nocce <tiagonoccecontato@gmail.com>
2026-09-01 13:06:32 -0300

    feat: implement firebase authentication and secure routing with dedicated auth and app pages

--- public/app.js
+++ public/app.js
@@ -1,12 +1,5 @@
 // ==========================================
 // CONFIGURAÇÃO DO FIREBASE
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAywzg4yDP0p15RpnFwPc2Y2MGoT2U5l4M",
